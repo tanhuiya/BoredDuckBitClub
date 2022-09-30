37c7101d3afaf70fbb4b60d37711027ce878f289
tanhui <tanhui.master@gmail.com>
2022-09-30 16:18:52 +0800

    fix mint num

--- src/App.js
+++ src/App.js
@@ -94,7 +94,7 @@ function App() {
   const data = useSelector((state) => state.data);
   const [claimingNft, setClaimingNft] = useState(false);
   const [feedback, setFeedback] = useState(`Click buy to mint your NFT.`);
-  const [mintAmount, setMintAmount] = useState(1);
+  const [mintAmount, setMintAmount] = useState(10);
   const [CONFIG, SET_CONFIG] = useState({
     CONTRACT_ADDRESS: "",
     SCAN_LINK: "",
